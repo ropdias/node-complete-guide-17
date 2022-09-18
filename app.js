d45ce725f05f7c7b4c29ba05e49133613a4ce38a
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-09-18 17:57:24 -0300

    Removing forwarding parameters to the include in ejs files. Any data available to the main template is also available to any other templates nested in it. We should only forward data for local variables.

--- app.js
+++ app.js
@@ -122,7 +122,6 @@ app.use((error, req, res, next) => {
   // res.redirect("/500"); // This can lead to infinite loop if you thrown an Error in SYNC code
   // We can also render a page here or return some JSON data here
   // res.status(error.httpStatusCode).render(...);
-  console.log(error);
   res.status(500).render("500", {
     pageTitle: "Error!",
     path: "/500",
